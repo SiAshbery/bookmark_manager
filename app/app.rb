63634cafe8b67e4e3ec9fae97f685e2e06c7877f
charliefea <charlotte.feather@yahoo.com>
2017-06-02 15:47:58 +0100

    Challenge 19

--- app/app.rb
+++ app/app.rb
@@ -51,10 +51,7 @@ end
   end
 
   post "/new_user" do
-    # password = BCrypt::Password.create(params[:password])
-    user = User.create(email: params[:email], password: params[:password])
+    user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
-    redirect "/links"
   end
-
 end
--- spec/feature/creating_user_accounts_spec.rb
+++ spec/feature/creating_user_accounts_spec.rb
@@ -2,13 +2,20 @@
 
 feature "Creating user account" do
   scenario "After filling out a completing signup form a user account is created" do
-    visit "/signup"
-    fill_in "email", with: "fake@email.com"
-    fill_in "password", with: "password"
-    click_button "submit"
-    expect(page).to have_content "Welcome fake@email.com!"
+    expect{ signup}.to change(User, :count )
   end
-  scenario "Increases amount of users by 1" do
-    expect{ User.create }.to change { User.count }.by(1)
+
+  scenario "Confirm password must match password" do
+    expect{ signup(password_confirmation: "notpassword")}.not_to change(User, :count )
+  end
+
+  def signup(email: "fake@email.com",
+            password:"password",
+            password_confirmation: "password")
+    visit "/signup"
+    fill_in "email", with: email
+    fill_in "password", with: password
+    fill_in "password_confirmation", with: password_confirmation
+    click_button "submit"
   end
 end
