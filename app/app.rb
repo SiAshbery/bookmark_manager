dcdf058e1286a1551811589812be87dd66346c89
Simon Ashbery <si.ashbery@gmail.com>
2017-06-02 14:23:15 +0100

    Encrypted user password

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,20 @@
-
 ENV['RACK_ENV'] ||= "development"
 
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require 'bcrypt'
 
 
 class BookmarkManager < Sinatra::Base
+
+helpers do
+  def current_user
+   @current_user ||= User.get(session[:user_id])
+  end
+end
+
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     redirect '/links'
@@ -42,9 +51,10 @@ class BookmarkManager < Sinatra::Base
   end
 
   post "/new_user" do
-    User.create(email: params[:email], password: params[:password])
+    # password = BCrypt::Password.create(params[:password])
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
     redirect "/links"
-
   end
 
 end
